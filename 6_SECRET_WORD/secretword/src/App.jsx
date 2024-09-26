bcf1bb814ac0ca107435f69176df137e7923f9fe
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-09-25 23:27:00 -0300

    Consuming states of game

--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -26,6 +26,11 @@ const [pickedWord, setPickedWord] = useState('')
 const [pickedCategory, setPickedCategory] = useState('')
 const [letters, setLetters] = useState([])
 
+const [guessedLetters, setGuessedLetters] = useState([])
+const [wrongLetters, setWrongLetters] = useState([])
+const [guesses, setGuesses] = useState(3)
+const [score, setScore] = useState(0)
+
 const pickWordAndCategory = () => {
   // pick a random category
   const categories = Object.keys(words)
@@ -58,7 +63,7 @@ const startGame = () => {
   // fill states
   setPickedWord(word)
   setPickedCategory(category)
-  setLetters(letters)
+  setLetters(wordLetters)
 
   
   setGameStage(stages[1].name)
@@ -79,7 +84,16 @@ const retry = () =>{
   return (
     <div className='App'>
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'game' && <Game 
+      verifyLetter={verifyLetter} 
+      pickedWord={pickedWord} 
+      pickedCategory={pickedCategory} 
+      letters={letters}
+      guessedLetters={guessedLetters}
+      wrongLetters={wrongLetters} 
+      guesses={guesses}
+      score={score}
+      />}
       {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   )
