35b292fd9c419751c2141cf014f46d63897647be
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-09-26 01:24:48 -0300

    Letter insertion event

--- 6_SECRET_WORD/secretword/src/components/Game.jsx
+++ 6_SECRET_WORD/secretword/src/components/Game.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react'
 import './Game.css'
 
 const Game = ({
@@ -9,6 +10,18 @@ const Game = ({
   wrongLetters, 
   guesses, 
   score}) => {
+    const [letter, setLetter] = useState('')
+    const letterInputRef = useRef(null)
+
+    const handleSubmit = (e) =>{
+      e.preventDefault()
+
+      verifyLetter(letter)
+      setLetter('')
+
+      letterInputRef.current.focus()
+    }
+
   return (
     <div className='game'>
        <p className="points">
@@ -30,8 +43,15 @@ const Game = ({
        </div>
        <div className="letterContainer">
         <p>Tente adivinhar uma letra da palavra:</p>
-        <form>
-          <input type="text" name='letter' maxLength='1' required />
+        <form onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          name='letter' 
+          maxLength='1' 
+          required 
+          onChange={(e) => setLetter(e.target.value)}
+          value={letter}
+          ref={letterInputRef} />
           <button>Jogar!</button>
         </form>
        </div>
